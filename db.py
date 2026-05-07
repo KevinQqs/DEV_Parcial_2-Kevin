1128c4cdd8d29acf969bf880b2e1fc3ced4e1545
KevinQqs <ksquinones09@ucatolica.edu.co>
2026-05-07 14:24:13 -0500

    [ADD] DB Conecction

--- db.py
+++ db.py
@@ -1,10 +1,21 @@
 import os
+
 from sqlmodel import Session, create_engine, SQLModel
 from fastapi import FastAPI, Depends
 from typing import Annotated
+from dotenv import load_dotenv
 
 
-engine = create_engine()
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True
+)
+
 
 def create_all_tables(app: FastAPI):
     if os.getenv("ENV") == "dev":
@@ -12,10 +23,9 @@ def create_all_tables(app: FastAPI):
     yield
 
 
-def get_session()->Session:
+def get_session() -> Session:
     with Session(engine) as session:
-        yield session
+        yield
 
 
 SessionDep = Annotated[Session, Depends(get_session)]
-
