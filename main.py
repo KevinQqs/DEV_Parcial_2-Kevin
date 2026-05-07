71c79644c43341138c372ba46ba5cf7c344b1272
KevinQqs <ksquinones09@ucatolica.edu.co>
2026-05-07 14:25:25 -0500

    [UPDATE] MAIN

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from typing import List
 
 from fastapi import FastAPI
-
-from db import  SessionDep, create_all_tables
 from sqlmodel import select
 
-app = FastAPI()
+from db import SessionDep, create_all_tables
+from models import Dog
 
 
-
+app = FastAPI(
+    lifespan=create_all_tables
+)
 
 @app.get("/")
 async def root():
@@ -18,3 +19,31 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.post("/dogs", response_model=Dog)
+def create_dog(dog: Dog, session: SessionDep):
+
+    session.add(dog)
+    session.commit()
+    session.refresh(dog)
+
+    return dog
+
+
+@app.get("/dogs", response_model=List[Dog])
+def get_dogs(session: SessionDep):
+
+    dogs = session.exec(
+        select(Dog)
+    ).all()
+
+    return dogs
+
+
+@app.get("/dogs/{dog_id}", response_model=Dog)
+def get_dog(dog_id: int, session: SessionDep):
+
+    dog = session.get(Dog, dog_id)
+
+    return dog
