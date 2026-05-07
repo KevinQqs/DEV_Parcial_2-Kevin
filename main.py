b8ab75c996538044a267140190bf2fb38120b756
KevinQqs <ksquinones09@ucatolica.edu.co>
2026-05-07 14:38:05 -0500

    [UPDATE] MAIN ENDPOINTS

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from sqlmodel import select
 
 from db import SessionDep, create_all_tables
-from model import Dog, DogCreate, DogRead
 
+from model import (
+    Dog,
+    DogCreate,
+    DogRead,
+    DogUpdate
+)
 
 app = FastAPI(
     lifespan=create_all_tables
@@ -13,24 +18,11 @@ app = FastAPI(
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "API funcionando"}
 
 
-# Crear perro
-@app.post("/dogs", response_model=DogRead)
-def create_dog(dog: DogCreate, session: SessionDep):
-
-    db_dog = Dog.model_validate(dog)
-
-    session.add(db_dog)
-    session.commit()
-    session.refresh(db_dog)
-
-    return db_dog
-
-
-# Obtener todos
+# OBTENER TODOS
 @app.get("/dogs", response_model=List[DogRead])
 def get_dogs(session: SessionDep):
 
@@ -41,10 +33,60 @@ def get_dogs(session: SessionDep):
     return dogs
 
 
-# Obtener por id
+# OBTENER POR ID
 @app.get("/dogs/{dog_id}", response_model=DogRead)
 def get_dog(dog_id: int, session: SessionDep):
 
     dog = session.get(Dog, dog_id)
 
+    if not dog:
+        raise HTTPException(
+            status_code=404,
+            detail="Perro no encontrado"
+        )
+
     return dog
+
+
+# CREAR
+@app.post("/dogs", response_model=DogRead)
+def create_dog(
+        dog: DogCreate,
+        session: SessionDep
+):
+
+    db_dog = Dog.model_validate(dog)
+
+    session.add(db_dog)
+    session.commit()
+    session.refresh(db_dog)
+
+    return db_dog
+
+
+# ACTUALIZAR
+@app.put("/dogs/{dog_id}", response_model=DogRead)
+def update_dog(
+        dog_id: int,
+        dog_update: DogUpdate,
+        session: SessionDep
+):
+
+    db_dog = session.get(Dog, dog_id)
+
+    if not db_dog:
+        raise HTTPException(
+            status_code=404,
+            detail="Perro no encontrado"
+        )
+
+    dog_data = dog_update.model_dump(exclude_unset=True)
+
+    for key, value in dog_data.items():
+        setattr(db_dog, key, value)
+
+    session.add(db_dog)
+    session.commit()
+    session.refresh(db_dog)
+
+    return db_dog
