943781001349310fa01109d6f35fc2d48aba2e79
Mateocastro428 <cmcastro47@ucatolica.edu.co>
2026-05-07 14:16:21 -0500

    [ADD] MODEL DOG

--- model.py
+++ model.py
@@ -1,7 +1,16 @@
-from sqlmodel import Field, SQLModel
-
+from sqlmodel import SQLModel, Field
 from datetime import datetime
+from typing import Optional
 
+
+class Dog(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    size: str
+    dangerous: bool
+    sterilized: bool
+    breed: str
+    created: datetime = Field(default_factory=datetime.now)
 
 class Dog(SQLModel, table = True):
     __tablename__ = "Dogs"
